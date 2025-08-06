ed55dd942c1c525b024b13aa727e2cbe38f5b29d
abdullaHanbali <abdullahanbali97@gmail.com>
2025-08-07 01:04:34 +0300

    update

--- src/SmartBuyAutomation/TestCases.java
+++ src/SmartBuyAutomation/TestCases.java
@@ -16,30 +16,34 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestCases {
+
+	WebDriver driver = new ChromeDriver();
 	
-	WebDriver driver = new ChromeDriver();	
 	String TheURL = "https://smartbuy-me.com/";
-	
+
 	Random rand = new Random();
-	
+
 	@BeforeTest
 	public void mySetup() {
+		
 		driver.get(TheURL);
+		
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test(priority = 1)
 	public void homePageAccessability() throws InterruptedException {
-		
+
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//*[@id='vitals_popup']//button[2]")).click();
 		
-		JavascriptExecutor js = (JavascriptExecutor) driver; 
+		driver.findElement(By.xpath("//*[@id='vitals_popup']//button[2]")).click();
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		// Scroll down gradually to bottom
 		long height = (long) js.executeScript("return document.body.scrollHeight");
 		for (int i = 0; i < height; i += 100) {
-		    js.executeScript("window.scrollBy(0, 100);");
-		    Thread.sleep(100); 
+			js.executeScript("window.scrollBy(0, 100);");
+			Thread.sleep(100);
 		}
 
 		// Wait 2 seconds at bottom
@@ -47,19 +51,68 @@ public class TestCases {
 
 		// Scroll up gradually to top
 		for (int i = 0; i < height; i += 100) {
-		    js.executeScript("window.scrollBy(0, -100);");
-		    Thread.sleep(100);
+			js.executeScript("window.scrollBy(0, -100);");
+			Thread.sleep(100);
 		}
 	}
-	
+
 	@Test(priority = 2)
-	public void ProductCategoryNavigation() {
+	public void ProductCategoryNavigation() throws InterruptedException {
+
+		Actions actions = new Actions(driver);
+
+		WebElement shopByCategory = driver.findElement(By
+		.xpath("//*[@id=\"shopify-section-sections--23554635530550__header\"]/section/nav/div/div/ul/li[2]/a"));
+		actions.moveToElement(shopByCategory).click().build().perform();
+
+		Thread.sleep(2000);
+
+		List<WebElement> subCategories = driver.findElements(By.cssSelector("a.nav-dropdown__link.menu-toggle"));
+
+		int randomIndex = new Random().nextInt(subCategories.size());
+		WebElement randomSubCategory = subCategories.get(randomIndex);
+
+		actions.moveToElement(randomSubCategory).click().build().perform();
 		
-	
+		Thread.sleep(2000);
+		
+		actions.moveToElement(randomSubCategory).click().build().perform();
+
+		Thread.sleep(1000);
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		// Scroll down gradually to bottom
+		long height = (long) js.executeScript("return document.body.scrollHeight");
+		for (int i = 0; i < height; i += 100) {
+			js.executeScript("window.scrollBy(0, 100);");
+			Thread.sleep(100);
+		}
+
+		// Wait 2 seconds at bottom
+		Thread.sleep(1000);
+
+		// Scroll up gradually to top
+		for (int i = 0; i < height; i += 100) {
+			js.executeScript("window.scrollBy(0, -100);");
+			Thread.sleep(100);
+		}
+		
+		Thread.sleep(1000);
 	}
 	
+	@Test(priority = 3)
+	public void ProductSearchFunctionality() throws InterruptedException {
+		
+		driver.findElement(By.name("q")).sendKeys("iphone 14 plus");
+		
+		Thread.sleep(1000);
+		
+		driver.findElement(By.className("search-bar__submit")).click();
+	
+	}
+
 	@AfterTest
 	public void tearDown() {
 
-    }
+	}
 }
